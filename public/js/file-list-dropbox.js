ba3f0d79f80c4d01decf35c0b43cc15c6c9f71ef
dkl0pjh <pheslin@gmail.com>
2019-11-23 12:49:30 +0000

    Partial start of new dropbox listing code

--- public/js/file-list-dropbox.js
+++ public/js/file-list-dropbox.js
@@ -3,19 +3,70 @@ var fileList = []
 var token = localStorage.getItem("access_token")
 var dbx = new Dropbox.Dropbox({ accessToken: token });
 console.log('dbx', dbx)
-refreshListDropbox()
+
+// showSavedDropbox()
 
 
 function refreshListDropbox () {
-  dbx.filesListFolder({path: '', recursive: true, include_deleted: false})
-  .then(function(response) {
-    console.log('response', response)
-    displayFiles(response.entries);
-    console.log(response);
+  localStorage.removeItem('DropboxList')
+  Promise.all([getListDropbox(), getMetadata()])
+  .then(function(responses) {
+    var list = responses[0].entries
+    var data = responses[1]
+    
   })
-  .catch(function(error) {
-    console.error(error);
-  });
+}
+
+function getListDropbox () {
+  // Returns a promise
+  return dbx.filesListFolder({path: '', recursive: true, include_deleted: false})
+}
+//   .then(function(response) {
+//     console.log('response', response)
+//     saveListDropbox(response.entries);
+//     // console.log(response);
+//   })
+//   .catch(function(error) {
+//     console.error(error);
+//   });
+// }
+
+function getMetadata () {
+  return new Promise(function (resolve, reject) {
+    var req = new XMLHttpRequest()
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.responseType = 'json'
+    req.onreadystatechange = function () {
+      if (request.readyState !== 4) return;
+      if (request.status >= 200 && request.status < 300) {
+        resolve(JSON.parse(request.responseText))
+      } else {
+        // If failed
+        reject({
+          status: request.status,
+          statusText: request.statusText
+        })
+      }
+    }
+    var url = window.location.origin + '/getMetadata' + "?user=" + localStorage.getItem("user")
+    // console.log(url)
+    req.open("GET", url);
+    req.send();
+}
+
+function saveListDropbox (files) {
+  var data = getMetadata(files)
+  // for (var i = 0; i < files.length; i++) {
+  //   var author = getAuthor(files[i])
+  //   var title = getTitle(files[i])
+  // }
+  // showSavedDropbox()
+}
+
+
+
+function showSavedDropbox () {
+  var entries = localStorage.getItem('DropboxList')
 }
 
 function displayFiles(files) {
@@ -25,7 +76,7 @@ function displayFiles(files) {
      li = document.createElement('li');
      var href = window.location.origin + '/fileDisplay' +
      "?user=" + localStorage.getItem("user") +
-     '&host=dropbox' + 
+     '&host=dropbox' +
      '&filePath=' + files[i].path_lower
      var a = document.createElement('a')
      a.setAttribute('href', href)
--- todo.md
+++ todo.md
@@ -1,13 +1,19 @@
 # English dictionary for transl and parse_eng
-# Refactor dropbox connection
 # Serve author and text names
 # Test on tablet
+
+# Ensure Perseus server enforces and records user type
 
 # Add Gentium
 
 # Fix bugs
 - Logeion link
 - Get rid of OLD/TLL links in Perseus output
+
+# Before release
+- Move assets to CDN
+Put all XML files, json and static js/css assets into a bucket.
+Keep even static html on node server to record user type
 
 # Longer term
 - Add search server

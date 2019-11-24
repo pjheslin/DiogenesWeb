6406060d5a0ae1a226e217dd96e6a4d34ad943ff
dkl0pjh <pheslin@gmail.com>
2019-11-24 20:00:04 +0000

    Functioning dropbox file list

--- public/js/file-list-dropbox.js
+++ public/js/file-list-dropbox.js
@@ -5,84 +5,161 @@ var dbx = new Dropbox.Dropbox({ accessToken: token });
 console.log('dbx', dbx)
 
 // showSavedDropbox()
+getListDropbox()
 
-
-function refreshListDropbox () {
-  localStorage.removeItem('DropboxList')
-  Promise.all([getListDropbox(), getMetadata()])
-  .then(function(responses) {
-    var list = responses[0].entries
-    var data = responses[1]
-    
-  })
-}
 
 function getListDropbox () {
-  // Returns a promise
   return dbx.filesListFolder({path: '', recursive: true, include_deleted: false})
+  .then (function(response) {
+    console.log('response', response)
+    // console.log(response.entries);
+    // Is path_display the right feature to extract?
+    var filenames = response.entries.map(file => file.path_display)
+    return getMetadata(filenames);
+  })
+  .then (function (metadata) {
+    console.log(metadata)
+    sortFiles(metadata)
+    //??
+  })
+  .catch (function(error) {
+    console.error(error);
+  });
 }
-//   .then(function(response) {
-//     console.log('response', response)
-//     saveListDropbox(response.entries);
-//     // console.log(response);
-//   })
-//   .catch(function(error) {
-//     console.error(error);
-//   });
-// }
 
-function getMetadata () {
+function getMetadata (files) {
   return new Promise(function (resolve, reject) {
     var req = new XMLHttpRequest()
-    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    req.responseType = 'json'
     req.onreadystatechange = function () {
-      if (request.readyState !== 4) return;
-      if (request.status >= 200 && request.status < 300) {
-        resolve(JSON.parse(request.responseText))
+      if (req.readyState !== 4) return;
+      if (req.status >= 200 && req.status < 300) {
+        console.log(req.response)
+        resolve(JSON.parse(req.response))
       } else {
         // If failed
         reject({
-          status: request.status,
-          statusText: request.statusText
+          status: req.status,
+          statusText: req.statusText
         })
       }
     }
     var url = window.location.origin + '/getMetadata' + "?user=" + localStorage.getItem("user")
     // console.log(url)
-    req.open("GET", url);
-    req.send();
+    req.open("POST", url);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.responseType = 'json'
+    req.send(JSON.stringify(files));
+  })
 }
 
-function saveListDropbox (files) {
-  var data = getMetadata(files)
-  // for (var i = 0; i < files.length; i++) {
-  //   var author = getAuthor(files[i])
-  //   var title = getTitle(files[i])
-  // }
-  // showSavedDropbox()
+// For Perl-style autovivification
+var tree = () => new Proxy({}, { get: (target, name) => name in target ? target[name] : target[name] = tree() })
+
+// Test:
+function sortFiles (files) {
+  var presortObj = tree()
+  for (var i = 0; i < files.length; i++) {
+    console.log(files[1])
+    var [filename, author, work] = files[i]
+    var corpus = filename.match(/([a-z]+)\d+\.xml$/)[1]
+    if (author == "???" & work == "???") {
+      corpus = "unknown"
+      author = filename
+    }
+    console.log(author, work, corpus)
+    presortObj[corpus][author][work] = filename
+  }
+  console.log(presortObj)
+  var sortedArr = []
+  var unknownArr
+  Object.keys(presortObj).sort().forEach(function (corpus) {
+    var corpusArr = [corpus]
+    if (corpus == "unknown") {
+      unknownArr = Object.keys(presortObj[corpus]).sort()
+      return
+    }
+    Object.keys(presortObj[corpus]).sort().forEach(function (author) {
+      var authorArr = [author]
+      Object.keys(presortObj[corpus][author]).sort().forEach(function (work) {
+        var workArr = [work, presortObj[corpus][author][work]]
+        console.log(JSON.stringify(authorArr))
+        authorArr.push(workArr)
+        // authorArr[1] = workArr
+        console.log(JSON.stringify(authorArr))
+      })
+      corpusArr.push(authorArr)
+      console.log(JSON.stringify(corpusArr))
+    })
+    console.log(corpusArr)
+    console.log(JSON.stringify(sortedArr))
+    sortedArr.push(corpusArr)
+    console.log(JSON.stringify(sortedArr))
+  })
+  if (unknownArr) {
+    sortedArr.push(unknownArr)
+  }
+  console.log(JSON.stringify(sortedArr))
+
+  showSavedDropbox(sortedArr)
+  localStorage.setItem('dropboxFileList', JSON.stringify(sortedArr))
 }
 
-
-
-function showSavedDropbox () {
-  var entries = localStorage.getItem('DropboxList')
+const corpusNames = {
+  phi: "PHI Latin Texts",
+  tlg: "TLG Texts",
+  misc: "Misc PHI Texts",
+  unknown: "Texts of Unknown Provenance"
 }
 
-function displayFiles(files) {
-   var filesList = document.getElementById('files');
-   var li;
-   for (var i = 0; i < files.length; i++) {
-     li = document.createElement('li');
-     var href = window.location.origin + '/fileDisplay' +
-     "?user=" + localStorage.getItem("user") +
-     '&host=dropbox' +
-     '&filePath=' + files[i].path_lower
-     var a = document.createElement('a')
-     a.setAttribute('href', href)
-     a.appendChild(document.createTextNode(files[i].name))
-     li.appendChild(a)
-     //li.appendChild(document.createTextNode(files[i].name));
-     filesList.appendChild(li);
-   }
- }
+function showSavedDropbox (sortedArr) {
+  if (!sortedArr) {
+    sortedArr = JSON.parse(localStorage.getItem('dropboxFileList'))
+  }
+  if (!sortedArr) {console.log('Missing dropboxFileList!')}
+  console.log(JSON.stringify(sortedArr))
+  var html = ''
+  sortedArr.forEach(corpusArr => {
+    var corpus = corpusArr.shift()
+    corpus_display = corpusNames[corpus];
+    html += `<button type="button" class="collapsible">${corpus_display}</button>\n`
+    html += `<div type="corpus" class="content">\n`
+    if (corpus == "unknown") {
+      corpusArr.foreach(filename => {
+        //??
+      })
+      return
+    }
+    corpusArr.forEach(authorArr => {
+      var author = authorArr.shift()
+      html += `<button type="button" class="collapsible authorName">${author}</button>\n`
+      html += `<div type="author" class="content">\n`
+      authorArr.forEach(workArr => {
+        var [work, filename] = workArr
+        html += `<button type="button" class="uncollapsible fileName"  onClick="openDropboxFile('${filename}')">${work}</button>\n`
+      })
+      html += `</div>\n`
+    })
+    html += `</div>\n`
+  })
+  var fileList = document.getElementById('fileList');
+  fileList.innerHTML = html
+  setupFolding()
+}
+
+function openDropboxFile (path) {
+  console.log(path)
+  var body = document.getElementsByTagName("BODY")[0];
+  body.classList.add("waiting");
+
+  var href
+  if (localStorage.getItem("user")) {
+    href = window.location.origin + '/fileDisplay' +
+    "?user=" + localStorage.getItem("user") +
+    '&host=dropbox' +
+    '&filePath=' + path
+  }
+  else {
+    href = window.location.origin + '/'
+  }
+  window.location.href = href
+}

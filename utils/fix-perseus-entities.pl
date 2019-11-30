a7b1459e141ccb7cc8e8d872749821b97f54e452
dkl0pjh <pheslin@gmail.com>
2019-12-01 03:18:02 +0800

    Move fix-perseus-entities

--- utils/fix-perseus-entities.pl
+++ utils/fix-perseus-entities.pl
@@ -15,7 +15,7 @@ use warnings;
 use utf8;
 use open qw(:utf8);
 
-chdir "../" or die $!;
+# chdir "../" or die $!;
 
 my %entities = (
 "&AElig;" => "Æ",
@@ -134,14 +134,14 @@ my %entities = (
 my @files = (`find public/texts/Perseus_Greek public/texts/Perseus_Latin -name "*[0-9].xml" -print |xargs pcregrep --buffer-size 16M -l "&[^;]+;|PersProse"`);
 
 foreach my $file (@files) {
-  print STDERR "$file\n";
+  # print STDERR "$file\n";
   local $/ = undef;
   open my $fh, "<$file" or die $!;
   my $out = '';
   while (my $chunk = <$fh>) {
     # $chunk =~ s/(&[A-Z\.a-z]+;)/{exists entities{$1} ? entities{$1} : die "No match for $1"}/gex;
     $chunk =~ s/(&[A-Z\.a-z]+;)/swap($1)/ge;
-    print ">$1\n" if $1;
+    # print ">$1\n" if $1;
     $chunk =~ s/\%PersProse;//g;
     $out .= $chunk;
   }
